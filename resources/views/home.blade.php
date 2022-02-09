f92f449c011be179ba3a6c2b6bf168ec5778197f
AlessioBottin <alessiobottin2002@gmail.com>
2022-02-09 16:51:51 +0100

    main

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,12 +1,35 @@
 @extends('layouts.app')
 
-@section('page-title')
+@section('page_title')
 Home
 @endsection
 
 @section('main_content')
-    <section>
-        
+
+    {{-- Banner  --}}
+    <section class="banner">
+        <div class="container">
+            <div class="banner-container">
+                <a href="#" class="btn btn-regular btn-blue">current series</a>
+            </div>
+        </div>
     </section>
+
+    {{-- Comics List  --}}
+    <div class="comic-list">
+        <div class="container">
+            <div class="comic-list-container">
+
+                @foreach ($comics as $comic)
+                    @include('components.comic-card', $comic)
+                @endforeach
+
+                <a href="#" class="btn btn-wide btn-blue">load more</a>
+
+            </div>
+        </div>
+    </div>
+
+
 @endsection
 
