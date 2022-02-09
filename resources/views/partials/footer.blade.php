fea177c2c4df8749cce6a7a2b998b1a6d6e1afe8
AlessioBottin <alessiobottin2002@gmail.com>
2022-02-09 17:24:09 +0100

    footer done

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,212 @@
-footer
+<footer>
+
+    {{-- Top  --}}
+    <div class="top-footer">
+        <div class="container">
+            <div class="footer-container">
+    
+                <!-- Link Container -->
+                <div class="link-container">
+                    
+                    <!-- Left  -->
+                    <div class="col col-l">
+    
+                        <!-- Comics  -->
+                        <div class="comics">
+    
+                            <!-- Title  -->
+                            <h3>dc shop</h3>
+    
+                            <!-- Link list  -->
+                            <ul>
+                                <a href="#">
+                                    <li>Character</li>
+                                </a>
+                                <a href="#">
+                                    <li>Comics</li>
+                                </a>
+                                <a href="#">
+                                    <li>Movies</li>
+                                </a>
+                                <a href="#">
+                                    <li>TV</li>
+                                </a>
+                                <a href="#">
+                                    <li>Games</li>
+                                </a>
+                                <a href="#">
+                                    <li>Videos</li>
+                                </a>
+                                <a href="#">
+                                    <li>News</li>
+                                </a>
+                            </ul>
+                        </div>
+    
+                        <!-- Shop  -->
+                        <div class="shop">
+    
+                            <!-- Title  -->
+                            <h3>shop</h3>
+    
+                            <!-- Link List  -->
+                            <ul>
+                                <a href="#">
+                                    <li>DC Shop</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>DC Collectibles</li>
+                                </a>
+                            </ul>
+    
+                        </div>
+    
+                    </div>
+    
+                    <!-- Center  -->
+                    <div class="col col-center">
+    
+                        <!-- Dc  -->
+                        <div class="comics">
+    
+                            <!-- Title  -->
+                            <h3>dc</h3>
+    
+                            <!-- Link list  -->
+                            <ul>
+                                <a href="#">
+                                    <li>Terms of use</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Priacy Policy</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Ad choices</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Advertising</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Jobs</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Subscriptions</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Talent workshop</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>CPSC Certificates</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Ratings</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Shop help</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>Contact us</li>
+                                </a>
+    
+                            </ul>
+    
+                        </div>
+    
+                    </div>
+    
+                    <!-- Right  -->
+                    <div class="col col-r">
+    
+                        <!-- Sites  -->
+                        <div class="sites">
+    
+                            <!-- Title  -->
+                            <h3>Sites</h3>
+    
+                            <!-- Link list  -->
+                            <ul>
+                                <a href="#">
+                                    <li>DC</li>
+                                </a>
+                                
+                                <a href="#">
+                                    <li>MAD Magazine</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>DC Kids</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>DC Universe</li>
+                                </a>
+    
+                                <a href="#">
+                                    <li>DC Power Visa</li>
+                                </a>
+                            </ul>
+    
+                        </div>
+    
+                    </div>
+    
+                </div>
+    
+                <!-- Image  -->
+                <div class="logo-container">
+                    <img src="../images/dc-logo-bg.png" alt="dc logo">
+                </div>
+    
+            </div>
+        </div>
+    </div>
+
+    {{-- Social  --}}
+    <div class="social-footer">
+        <div class="container">
+            <div class="social-footer-container">
+
+                <!-- Sign up button  -->
+                <div class="sign-up">
+                    <a href="#" class="btn btn-regular btn-transparent">sign up now!</a>
+                </div>
+
+                <!-- Social  -->
+                <div class="social">
+                    <div class="cta">follow us</div>
+
+                    <div class="social-list">
+                        <ul>
+                            <li>
+                                <img src="../images/footer-facebook.png" alt="fb logo">
+                            </li>
+                            <li>
+                                <img src="../images/footer-twitter.png" alt="twitter logo">
+                            </li>
+                            <li>
+                                <img src="../images/footer-youtube.png" alt="yt logo">
+                            </li>
+                            <li>
+                                <img src="../images/footer-pinterest.png" alt="pinterest logo">
+                            </li>
+                            <li>
+                                <img src="../images/footer-periscope.png" alt="periscope logo">
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
